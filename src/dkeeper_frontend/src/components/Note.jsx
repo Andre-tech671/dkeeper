c144c45532b35f95ff719f3c18d8c56a0c9169d8
Andre-tech671 <andre@example.com>
2026-01-20 09:33:29 +0200

    add delete button with trash icon
    - Integrated react-icons to display a trash icon on delete button
    - Improved UI/UX for note deletion with cleaner styling

--- src/dkeeper_frontend/src/components/Note.jsx
+++ src/dkeeper_frontend/src/components/Note.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { FaTrash } from "react-icons/fa";
 
 function Note(props) {
-  function handleClick() {
-    props.onDelete(props.id);
-  }
-
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={() => props.onDelete(props.id)}>
+        <FaTrash />
+      </button>
     </div>
   );
 }
